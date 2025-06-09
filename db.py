535959ae24e8c93961332afb5c1a0545c5d06ce5
MarianaBraga13 <maribragad@gmail.com>
2025-06-09 16:44:55 -0300

    Primeiro commit - Sistema de Login e CRUD com Tkinter

---
+++ db.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+#forma mais comum de criar um banco de dados
+#criar uma função para conectar e criar ao mesmo tempo o banco
+def conectar():
+    return sqlite3.connect("loja.db")
+
+#criar uma função para inserir tabelas
+def criar_tabs():
+    #criar um executor, no caso o cursor
+    #antes é necessário armazenar a função conectar() em uma variável
+    #porque vamos reutiliza-la para fechar a conexão
+    conn = conectar()
+    #agora crio o cursor que vai manipular os dados
+    #e já o armazeno conectado
+    cursor = conn.cursor()
+    #criando as tabelas
+    cursor.execute("""
+            CREATE IF NOT EXISTS usuario (
+                   id PRIMARY KEY AUTOINCREMENT,
+                   nome TEXT NOT NULL,
+                   senha TEXT NOT NULL
+                    );
+    """)
+    cursor.execute("""
+            CREATE IF NOT EXISTS produto (
+                   id PRIMARY KEY AUTOINCREMENT,
+                   nome TEXT NOT NULL,
+                   preco REAL NOT NULL
+                   );
+    """)
+
+    #salvando e fechando a conexão
+    cursor.commit()
+    cursor.close()
