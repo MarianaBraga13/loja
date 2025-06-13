2a10a952194a71dcbffad815ee129221df665f81
MarianaBraga13 <maribragad@gmail.com>
2025-06-13 15:04:57 -0300

    Atualizações nas buscas dos produtos e interface gráfica

--- produtos.py
+++ produtos.py
@@ -1,77 +1,109 @@
-# CRUD produtos
-
-# Buscar produtos
 import tkinter as tk
 from tkinter import messagebox
 from db import conectar
 
-# definindo a função de busca
-def carregar_produtos(lista, nome_entry):
+#definindo a função carregar/buscar
+
+def buscar_produtos(nome_entry, lista):
+    nome_busca = nome_entry.get().strip()
     conn = conectar()
     cursor = conn.cursor()
-    nome_da_busca = nome_entry.get().strip()
-    # banco de dados -> (SELECT) / front-end -> (nome_busca)
-    cursor.execute('SELECT * FROM produtos WHERE nome LIKE ?', (f'%{nome_da_busca}%',))
-
-    # procurando o nome ou parte dele dentro do banco de dados
-    # produtos_validos = "retorno de algo na busca", se sim, o cursor seleciona todos
-    produtos_validos = cursor.fetchall()
+    cursor.execute('SELECT * FROM produtos WHERE (nome) LIKE ? VALUE (?)', (f'%{nome_busca}%',))
+    # valor encontrado? ---> encontrou produtos?
+    # armazene em uma variável !
+    produtos_encontrados = cursor.fetchall()
     conn.close()
 
-    # back-end ---> front-end: mostrando na tela as opções de produtos ao usuário
-    if produtos_validos:
-        # criando a lista
-        for row in produtos_validos:
-            lista.insert(tk.END, f'{row[0]} - {row[1]} - R${row[2]:.2f}')
+    # quando os valores encontrados são armazenados em uma variável no back-end
+    # podemos então manipulá-los no back-end
+    # para podermos exibir uma lista no front-end, por exemplo
+
+    if produtos_encontrados: # ou seja, se o cursor nos traz algo do banco, então...
+        # agora precisamos exibir esses dados capturados pelo cursor.fetchall()
+        # eles estão armazenados na variável ---> produtos_encontrados
+        # exiba-os em uma lista
+        # como? percorra os resultados e crie uma nova tabela para o usuário
+        for coluna in produtos_encontrados:
+            lista.insert(f'{coluna[0]} - {coluna[1]} - R${coluna[2]}:.2f')
     else:
-        lista.insert(tk.END, "Nenhum produto encontrado.")
+        lista.insert(tk.END, "Nenhum produto encontrado")
+        # verificar se aqui ficaria melhor utiliar o messagebox
 
-#adicionar produtos
-
-def adicionar_produtos(nome_entry, preco_entry, lista):
-    #captar o valor digitado
+# adicionando produtos
+def incluir_produtos(nome_entry, preco_entry):
     nome = nome_entry.get()
     preco = preco_entry.get()
 
-    #resolvendo exceções
-    preco = float(preco)
+    # tratando exceção 1
     if not nome or not preco:
-        messagebox.showwarning("Atenção", "Preencha ambos os campos.")
-        return
-    
+        messagebox.showerror("Erro", "Ambos os campos devem ser preenchidos corretamente.")
+    # caso esteja correto ---> try it:
+
     try:
         conn = conectar()
         cursor = conn.cursor()
-        cursor.execute('INSERT INTO produtos (nome, preco) VALUES (?, ?)', (nome, preco))
-        conn.close()
-        messagebox.showinfo("Produto cadastrado com sucesso!")
+        # checando se já existe ou não no banco de dados
+        cursor.execute('SELECT * FROM produtos WHERE nome =?' (nome,))
+        existe = cursor.fetchone()
+
+        if not existe:
+            cursor.execute ('INSERT INTO produtos (nome, preco) VALUES (?, ?)', (nome, preco))
+            conn.close()
+            messagebox.showinfo("Info", f"Produto {nome} cadastrado com sucesso!")
+        else:
+            messagebox.showinfo("Info", "Produto já cadastrado.")
+
     except ValueError:
-        messagebox.showerror("Erro", "Produto inválido.")
+        messagebox.showerror("Erro", "Produto inválido. Não foi possível cadastrá-lo")
 
-    #limpar o produto digitado
-    carregar_produtos(lista)
-    nome_entry.delete(0, tk.END)
-    preco_entry.delete(0, tk.END)
+# deletando o produto
+def deletar_produto(nome_entry):
+    nome = nome_entry.get()
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM produtos WHERE nome=?'(nome,))
+    
+    existe = cursor.fetchone()
+    conn.close()
 
-# Importante! Faltou verificar se o produto existe ou não
+    if not existe:
+        messagebox.showinfo("Info", "Produto não existe no banco de dados.")
+    else:
+        cursor.execute('DELETE FROM produtos WHERE nome=?',(nome,))    
+    conn.commit()
+    conn.close()
 
-# deletar o produto (ele já foi carregado)
-# Imortante! Testar se o usuário consegue selecionar o que ele quer
-# Estudo: ver como ficaria o código se ele tivesse que digitar o nome inteiro do produto
-def deletar_produto(lista):
-    item = lista.get(tk.ACTIVE) # Importante! Aqui o ACTIVE deve estar deletando todos os produtos!Revisar.
-    if item:
-        # aqui preciso dizer para ele como ele acha o id do produto
-        # back-end
-        produto_id = int(item.split(" - ")[0])
-        # banco de dados
-        conn = conectar()
-        cursor = conn.cursor()
-        cursor.execute('DELETE FROM produtos WHERE id = ?', (produto_id))
-        conn.commit()
-        conn.close()
-        # Importante!Para quê preciso carregar a lista novamente no back-end? Revisar
-        carregar_produtos(lista)
+    # interface gráfica ---> front-end
+
+    def tela_principal():
+        root = tk.Tk()
+        root.title("Painel de Produtos")
+        root.geometry("400x500")
+
+        tk.Label(root, text="Nome do Produto").pack()
+        nome_entry = tk.Entry(root)
+        nome_entry.pack()
+
+        tk.Label(root, text="Preço").pack()
+        preco_entry = tk.Entry(root)
+        preco_entry.pack()
+
+        # Campo de busca
+        tk.Label(root, text="Buscar Produto por Nome").pack()
+        nome_entry = tk.Entry(root)
+        nome_entry.pack()
+        tk.Button(root, text="Buscar", command=lambda: buscar_produtos(nome_entry, lista))
+        lista = tk.Listbox(root, width=50)
+        lista.pack(pady=10)
+
+        tk.Button(root, text="Adicionar Produto", command=lambda:incluir_produtos(nome_entry, preco_entry))
+        tk.Button(root, text="Deletar Produto", command=lambda:deletar_produto(nome_entry))
+
+        buscar_produtos(lista)
+        root.mainloop()
+        
+
+        
 
 
 
