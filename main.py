dda5bfcf96fa449a504d49b0f73f994ab7a85c10
MarianaBraga13 <maribragad@gmail.com>
2025-06-14 17:44:35 -0300

    Atualização das observações para estudo.

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from db import criar_tabs
 from login import tela_login
 from produtos import tela_principal
 
+if __name__ == "__main__":
+    criar_tabs()
+    tela_login(tela_principal)
+
+# observações importantes:
+
 # estou passando tela_principal como parâmetro
 # para a tela_login
 # isso permite que se o acesso for liberado
 # a tela_principal seja acessada só depois do login
 
-if __name__ == "__main__":
-    criar_tabs()
-    tela_login(tela_principal)
-
+# se algum script importar o main, não executará nada que esteja após
+# a if __name__ == "__main__:" para não corrermos o risco de que
+# outros projetos comecem a executar funções desnecessárias
 
